e4198ef8d86ede2318e3e618a3446a07a56ae3ef
rrpronaldo <rrpronaldo@gmail.com>
2019-08-10 16:50:28 -0300

    Change 2.

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello Flask and OpenShift ..."
+    return "Terceito teste de deploy . . . </br> aa"
 
 if __name__ == "__main__":
     application.run()
